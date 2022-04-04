d328f84963fd621e5e144e1f71d50d77047826ea
huerqiang <13766876214@163.com>
2022-04-04 11:58:56 +0800

    first commit

---
+++ R/mitch.R
@@ -0,0 +1,13 @@
+#' mitch method
+#'
+#' @param multiGene a data.frame of multi-omics gene difference analysis results.
+#' @param TERM2GENE user input annotation of TERM TO GENE mapping,
+#' a data.frame of 2 column with term and gene
+#' @param minGSSize minimal size of genes annotated by Ontology term for testing.
+#' @param ... Other parameters.
+#' @noRd
+mitch_method <- function(multiGene, TERM2GENE, minGSSize, ...) {
+    PATHID2EXTID <- split(as.character(TERM2GENE[,2]), as.character(TERM2GENE[,1]))
+    mitch::mitch_calc(x = multiGene, genesets = PATHID2EXTID,
+        minsetsize = minGSSize, ...)
+}
